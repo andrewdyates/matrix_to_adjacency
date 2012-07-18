329d495bf28f2a5f3b246242d801bb15261dddd6
Andrew D. Yates <andrewyates.name@gmail.com>
2012-07-18 12:34:42 -0400

    abs value

--- script.py
+++ script.py
@@ -2,17 +2,22 @@
 """Convert matrix to adjacency list
 
 EXAMPLE USE:
-  python $HOME/matrix_to_adjacency/script.py npyfname $HOME/gse15745/gse15745_gpl8178_gpl6104_dcor.values.npy 0.7
+  python $HOME/matrix_to_adjacency/script.py npyfname=$HOME/gse15745/gse15745_gpl8178_gpl6104_dcor.values.npy thresh=0.6
 """
+import sys
+import numpy as np
 
-def main(npyfname, thresh):
+def main(npyfname, thresh, absvalue=False):
   thresh = float(thresh)
   M = np.load(npyfname)
+  if absvalue:
+    M = np.fabs(M)
   for i in xrange(np.size(M,0)):
-    s = " ".join(np.where(M[i,:]>=thresh))
-    print s
+    print " ".join([str(x) for x in np.where(M[i,:]>=thresh)[0]])
+
     
 
 
 if __name__ == "__main__":
-  main(**dict([s.split('=') for s in sys.argv[1:]]))
+  args = dict([s.split('=') for s in sys.argv[1:]])
+  main(**args)
