7d985836ae5d4e450414634bdd710becb61043cf
Andrew D. Yates <andrewyates.name@gmail.com>
2012-07-20 11:23:21 -0400

    fixed line printing

--- script.py
+++ script.py
@@ -3,7 +3,7 @@
 Filter rows that do not have any transations
 
 EXAMPLE USE:
-  python $HOME/matrix_to_adjacency/script.py npyfname=$HOME/gse15745/gse15745_gpl8178_gpl6104_dcor.values.npy thresh=0.6
+  python $HOME/matrix_to_adjacency/script.py npyfname=$HOME/gse15745/gse15745_gpl8178_gpl6104_dcor.values.npy threshold=0.4 
 """
 import sys
 from __init__ import *
@@ -12,7 +12,7 @@ def main(**kwds):
   empty_lines = []
   for line in npy_to_mafia(empty_lines, **kwds):
     print line
-  sys.stderr.write("\n".join(empty_lines))
+  sys.stderr.write("\n".join(map(str, empty_lines)))
   
       
 if __name__ == "__main__":
